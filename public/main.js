950bfb092e2c9a799877dbe38aa63e0376b512ec
Thomas Sankara <thomassankara74@gmail.com>
2021-05-12 19:16:01 +0300

    update electron entry file

--- public/main.js
+++ public/main.js
@@ -1,11 +1,17 @@
 const { app, BrowserWindow } = require("electron")
+const { path } = require("path")
+const isDev = require("electron-is-dev")
 
 function createWindow() {
   // Create the browser window
   win = new BrowserWindow({ width: 800, height: 600 })
 
   // Load your React App
-  win.loadURL("http://localhost:3000/")
+  win.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  )
 }
 
 app.on("ready", createWindow)
