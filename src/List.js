c097ad212835c8b1d85933e775b3a8b4bba6010b
Thomas Sankara <thomassankara74@gmail.com>
2021-05-12 19:17:07 +0300

    add delete and edit functions

--- src/List.js
+++ src/List.js
@@ -1,12 +1,7 @@
 import { FiCheckCircle } from "react-icons/fi"
 import { FaTrash, FaEdit } from "react-icons/fa"
 
-const List = ({ items, setItems }) => {
-  const deleteItem = (id) => {
-    const remainingItems = items.filter((item) => item.id !== id)
-    setItems(remainingItems)
-  }
-
+const List = ({ items, deleteItem, editItem }) => {
   return (
     <>
       <h3>You have {items.length} items in your Todo list</h3>
@@ -17,13 +12,13 @@ const List = ({ items, setItems }) => {
               <li key={id}>{title}</li>
             </ul>
             <div className="article-inner">
-              <button className="btnCheck">
+              <button className="btnCheck" onClick={() => deleteItem(id)}>
                 <FiCheckCircle />
               </button>
-              <button className="btnEdit">
+              <button className="btnEdit" onClick={() => editItem(id)}>
                 <FaEdit />
               </button>
-              <button className="btnDelete" onClick={(id) => deleteItem(id)}>
+              <button className="btnDelete" onClick={() => deleteItem(id)}>
                 <FaTrash />
               </button>
             </div>
