7b751ef4ddae9f1f50adcdfa3fc776755c328b9b
Thomas Sankara <thomassankara74@gmail.com>
2021-05-12 19:16:48 +0300

    disable unused vars, delete and edit functions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { v4 as uuidv4 } from "uuid"
 import { FaPlus } from "react-icons/fa"
 import List from "./List"
 import logo from "./logo.svg"
 
+const getLocalStorage = () => {
+  let items = localStorage.getItem("items")
+  if (items) {
+    return JSON.parse(localStorage.getItem("items"))
+  } else {
+    return []
+  }
+}
+
 const App = () => {
   const [text, setText] = useState("")
-  const [items, setItems] = useState([])
+  const [items, setItems] = useState(getLocalStorage())
+  // eslint-disable-next-line
+  const [isEditing, setIsEditing] = useState(false)
+  // eslint-disable-next-line
+  const [editID, setEditID] = useState(null)
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -17,10 +30,23 @@ const App = () => {
       const newList = { id: uuidv4(), title: text }
       setItems([newList, ...items])
       setText("")
-
-      console.log(newList.id)
     }
   }
+
+  const deleteItem = (id) => {
+    setItems(items.filter((item) => item.id !== id))
+  }
+
+  const editItem = (id) => {
+    const editingItem = items.find((item) => item.id === id)
+    setIsEditing(true)
+    setEditID(id)
+    setText(editingItem.title)
+  }
+
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items))
+  }, [items])
 
   return (
     <>
@@ -45,7 +71,20 @@ const App = () => {
             </button>
           </form>
 
-          <List items={items} setItems={setItems} />
+          <List
+            items={items}
+            setItems={setItems}
+            deleteItem={deleteItem}
+            editItem={editItem}
+          />
+
+          <button
+            className="btnAdd"
+            style={{ marginTop: 20 }}
+            onClick={() => setItems([])}
+          >
+            Clear List
+          </button>
         </section>
       </div>
     </>
