4e83dd9c83586821ccc59ead85364a468fd54f66
Thomas Sankara <thomassankara74@gmail.com>
2021-05-13 22:32:20 +0300

    update main electron file

--- public/main.js
+++ public/main.js
@@ -1,17 +1,60 @@
-const { app, BrowserWindow } = require("electron")
+const { app, BrowserWindow, Menu, dialog } = require("electron")
+const { powerMonitor } = require("electron/main")
 const { path } = require("path")
-const isDev = require("electron-is-dev")
 
 function createWindow() {
   // Create the browser window
-  win = new BrowserWindow({ width: 800, height: 600 })
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    minWidth: 600,
+    fullscreenable: true,
+    backgroundColor: "#f6d6d6",
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  })
 
   // Load your React App
-  win.loadURL(
-    isDev
-      ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "../build/index.html")}`
-  )
+  win.loadURL(`file://${path.join(__dirname, "../build/index.html")}`)
 }
 
-app.on("ready", createWindow)
+function menu() {
+  const template = [
+    {
+      label: "Nexus",
+      submenu: [
+        {
+          label: "About",
+          click: () => {
+            dialog.showMessageBox(win, {
+              type: "Info",
+              title: "About",
+              message: "Nexus is built by Thomas Sankara, @tsbsankara",
+              detail:
+                "Find him on Twitter @TSBSankara, GitHub @SankThomas or at https://tsbsankara.co.ke",
+            })
+          },
+        },
+        {
+          label: "Quit",
+          click: () => {
+            app.quit()
+          },
+        },
+      ],
+    },
+  ]
+
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+}
+
+app.on("ready", () => {
+  createWindow()
+  menu()
+
+  powerMonitor.on("resume", () => {
+    win.reload()
+  })
+})
